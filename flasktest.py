99b9658b2a5a8c0733c3a6e6e57a6202bf48e8ae
sgmtg <shanbenyan3@gmail.com>
2023-06-17 08:43:18 +0900

    initial

--- flasktest.py
+++ flasktest.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ def lambda_handler(event=None, context=None):
 @app.route('/<name>', methods=['GET', 'POST'])
 def hello_name(name):
     
-    return 'hello ' + name
+    return jsonify({'message': 'Account successfully created', 'user': name}), 200
 
 if __name__ == '__main__':
     app.run()
--- request.py
+++ request.py
@@ -1,6 +1,7 @@
 import requests
 
 url = 'https://lkhhg706o4.execute-api.ap-northeast-3.amazonaws.com/dev'
+url = 'https://cymntn2bea.execute-api.ap-northeast-3.amazonaws.com/dev' 
 
 response = requests.get(url)
 print(response.text)
