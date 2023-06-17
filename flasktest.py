90a4a6ae306cd7effa62f79ee0d8e25059500ec7
kkitagawaa <kitarira_1@yahoo.co.jp>
2023-06-17 17:13:05 +0900

    ワードクラウドのエンドポイント作成

--- flasktest.py
+++ flasktest.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import create_wordcloud
+
 
 app = Flask(__name__)
 
@@ -7,19 +9,16 @@ def lambda_handler(event=None, context=None):
     
     return 'hello!'
 
-@app.route('/<words>', methods=['GET', 'POST'])
-def get_words(words):
+@app.route('/word-cloud', methods=['GET', 'POST'])
+def get_words():
     #postされたワードで検索して、結果を返す
-    # search_words(words)
+    data = request.get_json()
+    kw_list = data.get("kw_list")
+    image = create_wordcloud.create_wordcloud(kw_list=kw_list)
+    print(type(image))
+    return jsonify({'message': 'Word cloud created!'}), 200
 
 
-
-
-    #結果からワードクラウドを作成
-    # create_wordcloud(words)
-
-    
-    return jsonify({'image': 'gazoufile', 'text': words}), 200
 
 if __name__ == '__main__':
     app.run()
