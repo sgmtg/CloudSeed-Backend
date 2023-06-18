b9a1574fbeaaa969c7e1b7e8de5250b515b39b2e
kkitagawaa <kitarira_1@yahoo.co.jp>
2023-06-18 11:29:20 +0900

    画像表示まで?

--- flasktest.py
+++ flasktest.py
@@ -1,23 +1,26 @@
 from flask import Flask, jsonify, request, make_response, send_file
 import create_wordcloud
+from flask_cors import CORS, cross_origin
 from io import BytesIO
 import os
 
 app = Flask(__name__)
-
+CORS(app)
 
 # ルートパスへのリクエストを処理する関数
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST", "OPTIONS"])
 def lambda_handler(event=None, context=None):
     return "hello!"
 
 
 # /word-cloud パスへのリクエストを処理する関数
-@app.route("/word-cloud", methods=["GET", "POST"])
+# @cross_origin(origins=["http://localhost:5173"], methods=["POST", "OPTIONS"])
+@app.route("/word-cloud", methods=["GET", "POST", "OPTIONS"])
 def get_words():
     # POSTリクエストからデータを取得
-    data = request.get_json()
-    kw_list = data.get("kw_list")
+    # data = request.get_json()
+    # kw_list = data.get("kw_list")
+    kw_list = ["key", "key2"]
 
     # ワードクラウドの生成
     # image = create_wordcloud.create_wordcloud(kw_list=kw_list)
@@ -26,9 +29,9 @@ def get_words():
     # img_io = BytesIO()
     # image.save(img_io, "JPEG", quality=95)
     # img_io.seek(0)
-    return send_file("/tmp/wordcloud.png", mimetype="image/jpeg")
+    return send_file("/tmp/wordcloud.png", mimetype="image/png")
 
 
 if __name__ == "__main__":
     # アプリケーションを実行する
-    app.run()
+    app.run(debug=True)
